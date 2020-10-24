642213c0eb3adb3c4b847ddee3d53c3529b7f2b7
Jennifer Kruk <jenkruk@gmail.com>
2020-10-24 15:07:55 -0700

    Material UI

--- material-ui-react/src/App.js
+++ material-ui-react/src/App.js
@@ -9,6 +9,48 @@ import Checkbox from '@material-ui/core/Checkbox';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import TextField from '@material-ui/core/TextField';
 
+import { makeStyles, ThemeProvider, createMuiTheme } from '@material-ui/core/styles'
+import { green, orange } from '@material-ui/core/colors';
+import 'fontsource-roboto';
+
+import Typography from '@material-ui/core/Typography'
+import Container from '@material-ui/core/Container'
+
+import Paper from '@material-ui/core/Paper'
+import Grid from '@material-ui/core/Grid'
+
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import IconButton from '@material-ui/core/IconButton'
+import MenuIcon from '@material-ui/icons/Menu'
+
+
+const useStyles = makeStyles({
+  root: {
+    background: 'linear-gradient(45deg, #FE6B8B, #FF8E53)',
+    border: 0,
+    marginBottom: 15, 
+    borderRadius: 15,
+    color: 'white',
+    padding: '5px 30px'
+  }
+})
+
+const theme = createMuiTheme ({
+  typography: {
+    h2: {
+      fontSize: 36,
+      marginBottom: 15,
+    }
+  }
+})
+
+
+function ButtonStyled() {
+  const classes = useStyles();
+  return <Button class = {classes.root}>Test Styled Button</Button>
+}
+
 function CheckboxExample() {
   const [checked, setChecked] = React.useState(true)
   return (
@@ -29,32 +71,60 @@ function CheckboxExample() {
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <TextField
-        variant="filled"
-        color="secondary"
-        type="email"
-        label="The Time"
-        placeholder="test@test.com"
-        />
-        <CheckboxExample/>
-        <ButtonGroup variant="contained" color="primary">
-        <Button
-          startIcon={<SaveIcon/>}
-          size="large">
-            Save
-          </Button>
-          <Button
-          startIcon={<DeleteIcon/>}
-          size="large"
-          variant="contained">
-            Discard
-          </Button>
-        </ButtonGroup>
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Container maxWidth="xs">
+        <div className="App">
+          <header className="App-header">
+            <AppBar color="secondary">
+              <Toolbar>
+                <IconButton>
+                  <MenuIcon />
+                </IconButton>
+                <Typography variant='h6'>
+                  MUI Themeing
+                </Typography>
+                <Button>
+                  Login
+                </Button>
+              </Toolbar>
+            </AppBar>
+            <Typography variant="h2" component="div">
+              Welcome to MUI
+            </Typography>
+            <Typography variant="subtitle1">
+              Learn how to use Material Ui
+            </Typography>
+            <ButtonStyled />
+            <Grid container spacing={2} justify="center">
+              <Grid item xs={3} sm={6}>
+                <Paper style={{ height: 75, width: '100%', }} />
+              </Grid>
+              <Grid item xs={3} sm={6}>
+                <Paper style={{ height: 75, width: '100%', }} />
+              </Grid>
+              <Grid item xs={3} sm={6}>
+                <Paper style={{ height: 75, width: '100%', }} />
+              </Grid>
+            </Grid>
+            <CheckboxExample/>
+            <ButtonGroup variant="contained" color="primary">
+            <Button
+              startIcon={<SaveIcon/>}
+              size="large">
+                Save
+              </Button>
+              <Button
+              startIcon={<DeleteIcon/>}
+              size="large"
+              variant="contained">
+                Discard
+              </Button>
+            </ButtonGroup>
+            <img src={logo} className="App-logo" alt="logo" />
+          </header>
+        </div>
+      </Container>
+    </ThemeProvider>
   );
 }
 
